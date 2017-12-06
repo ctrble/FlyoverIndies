37156839fb0e336517ac510f12bad43b3d5c6482
Charlotte Trible <ctrble@gmail.com>
2017-12-05 20:11:02 -0600

    adding hamburger functionality to the nav

--- react_spa/src/Main.js
+++ react_spa/src/Main.js
@@ -7,6 +7,8 @@ import {
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
+import scripts from './js/flyover-scripts.js'
+import logo from './img/text-logo.png';
 
 class Main extends Component {
 
@@ -16,9 +18,24 @@ class Main extends Component {
         <div>
           <h1>Flyover Indies</h1>
           <nav className='navbar is-fixed-top'>
-            <NavLink className='navbar-item' exact to='/'>Home</NavLink>
-            <NavLink className='navbar-item' to='/about'>About</NavLink>
-            <NavLink className='navbar-item' to='/contact'>Contact</NavLink>
+
+            <div className='navbar-brand'>
+              <NavLink className='navbar-item' exact to='/'><img src={logo} alt="Logo" /></NavLink>
+              <div className="navbar-burger burger" data-target="mainNavbar">
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+
+            <div id="mainNavbar" className="navbar-menu">
+              <div className="navbar-end">
+                <NavLink className='navbar-item' exact to='/'>Home</NavLink>
+                <NavLink className='navbar-item' to='/about'>About</NavLink>
+                <NavLink className='navbar-item' to='/contact'>Contact</NavLink>
+              </div>
+            </div>
+
           </nav>
           <div className='content'>
             <Route exact path='/' component={Home}/>
