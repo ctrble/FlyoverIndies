8150eb0a7b53ab0287d6c601c3c5d331a6eeb0d6
Charlotte Trible <ctrble@gmail.com>
2017-12-10 10:41:13 -0600

    testing tab component

--- react_spa/src/About.js
+++ react_spa/src/About.js
@@ -1,5 +1,18 @@
 import React, { Component } from 'react';
 
+class Tab extends React.Component {
+
+  render () {
+    return (
+      <div className='tabs'>
+        <ul>
+          <li>{this.props.text}</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
 class About extends React.Component {
 
   render() {
@@ -8,6 +21,8 @@ class About extends React.Component {
         <h1 className='title'>About</h1>
         <p>Flyover Indies is a collective of game developers based in Kansas City. We champion one another through our individual game development journeys and celebrate new voices in the KC game dev scene. Flyover Indies is medium agnostic. We support makers of all games: board, card, video, or whatever!</p>
         <p>Originally founded in 2014 under the name Crossroads Game Lab, and in 2017 we adopted our current name, Flyover Indies.</p>
+
+        <Tab text="FAQ" />
       </div>
      );
   }
