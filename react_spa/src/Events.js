a5fcb30c11030b0d52e7ffdf0beadf90fe8aab05
Charlotte Trible <ctrble@gmail.com>
2017-12-23 16:39:34 -0600

    cleanup of login

--- react_spa/src/Events.js
+++ react_spa/src/Events.js
@@ -5,10 +5,9 @@ class Events extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {isLoggedIn: false};
     this.checkLoginState = this.checkLoginState.bind(this);
     this.handleClick = this.handleClick.bind(this);
-    this.testAPI = this.testAPI.bind(this);
+    this.getEvents = this.getEvents.bind(this);
     this.statusChangeCallback = this.statusChangeCallback.bind(this);
   }
 
@@ -16,8 +15,7 @@ class Events extends React.Component {
     window.fbAsyncInit = function() {
       window.FB.init({
         appId: '1757685217787242', //random app number
-        cookie: true,  // enable cookies to allow the server to access
-                          // the session
+        cookie: true,  // enable cookies to allow the server to access the session
         xfbml: true,  // parse social plugins on this page
         version: 'v2.11',
         status: true,
@@ -25,14 +23,8 @@ class Events extends React.Component {
       //JS SDK initialized, now you can use it
       window.FB.XFBML.parse();
 
-      window.FB.Event.subscribe('auth.statusChange', function(response) {
-        if (response.authResponse) {
-          this.checkLoginState();
-          console.log('User is logged in.');
-        } else {
-          this.checkLoginState();
-          console.log('User is not logged in.');
-        }
+      window.FB.Event.subscribe('auth.authResponseChange', function(response) {
+        this.checkLoginState();
       }.bind(this));
     }.bind(this);
 
@@ -46,9 +38,6 @@ class Events extends React.Component {
     }(document, 'script', 'facebook-jssdk'));
   }
 
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
   checkLoginState() {
     window.FB.getLoginStatus(function(response) {
       this.statusChangeCallback(response);
@@ -56,87 +45,58 @@ class Events extends React.Component {
     }.bind(this));
   }
 
-  // This is called with the results from from FB.getLoginStatus().
   statusChangeCallback(response) {
     console.log('statusChangeCallback ' + response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
     if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      this.setState(prevState => ({
-        isLoggedIn: true
-      }));
-      this.testAPI();
+
+      this.getEvents();
+
+      window.FB.api('/me', function(response) {
+        if (response && !response.error) {
+          document.getElementById('status').innerHTML =
+            'Thanks for logging in, ' + response.name + '!';
+        }
+      });
 
     } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      this.setState(prevState => ({
-        isLoggedIn: false
-      }));
+
       document.getElementById('status').innerHTML = 'Please log ' +
         'into this app.';
     } else {
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-      this.setState(prevState => ({
-        isLoggedIn: false
-      }));
+
       document.getElementById('status').innerHTML = 'Please log ' +
       'into Facebook.';
     }
   }
 
   handleClick() {
-
     window.FB.getLoginStatus(function(response) {
       if (response.status === 'connected') {
-        // the user is logged in and has authenticated your
-        // app, and response.authResponse supplies
-        // the user's ID, a valid access token, a signed
-        // request, and the time the access token
-        // and signed request each expire
-        var uid = response.authResponse.userID;
-        var accessToken = response.authResponse.accessToken;
 
         console.log('Initiating log out');
         window.FB.logout();
 
       } else if (response.status === 'not_authorized') {
-        // the user is logged in to Facebook,
-        // but has not authenticated your app
+
+        console.log('Initiating log out of unauthorized app');
+        window.FB.logout();
+
       } else {
-        // the user isn't logged in to Facebook.
+
         console.log('Initiating log in');
         window.FB.login();
       }
-     });
+    });
   }
 
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
-  testAPI() {
-
-    window.FB.api('/me', function(response) {
-      if (response && !response.error) {
-
-        console.log('Successful login for: ' + response.name);
-        document.getElementById('status').innerHTML =
-          'Thanks for logging in, ' + response.name + '!';
-
-      } else {
-        console.log(response);
-      }
-    });
-
+  getEvents() {
     window.FB.api(
       '/1288441214569997/events',
       'GET',
       {},
       function (response) {
         if (response && !response.error) {
-          console.log(response);
+          console.log('events ' + response);
         }
       }
     );
@@ -149,7 +109,7 @@ class Events extends React.Component {
         <div id='status'></div>
 
         <button onClick={this.handleClick}>
-          {this.state.isLoggedIn ? 'Log Out' : 'Log In'}
+          Button
         </button>
 
       </div>
