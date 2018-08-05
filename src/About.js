e6828efbbf1594fc359e6a1a9276c44e589be781
Charlotte Trible <ctrble@gmail.com>
2018-08-05 11:12:05 -0500

    added sparkcade info

--- src/About.js
+++ src/About.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class About extends React.Component {
-
   render() {
     return (
       <div className="content">
-        <p className='title fi-title'>About Us</p>
-        <p>Flyover Indies is a collective of indie game developers based in Kansas City. We champion one another through our individual game development journeys and celebrate new voices in the KC game dev scene. Flyover Indies is medium agnostic. We support makers of all games: board, card, video, or whatever!</p>
-        <p>Originally founded in 2014 under the name Crossroads Game Lab, in 2017 we adopted our current name, Flyover Indies.</p>
+        <p className="title fi-title">About Us</p>
+        <p>
+          Flyover Indies is a community of indie game developers based in Kansas
+          City. We champion one another through our individual game development
+          journeys and celebrate new voices in the KC game dev scene. Flyover
+          Indies is medium agnostic. We support makers of all games: board,
+          card, video, or whatever!
+        </p>
+        <p>
+          Originally founded in 2014 under the name Crossroads Game Lab, in 2017
+          we adopted our current name, Flyover Indies.
+        </p>
       </div>
-     );
+    );
   }
 }
 
--- src/Events.js
+++ src/Events.js
@@ -1,70 +1,120 @@
-import React, { Component } from 'react';
-import Card from './components/Card';
-import devsAndDrinksImage from './img/events/devs-and-drinks.png';
-import studyHallImage from './img/events/study-hall.png';
-import coffeeIcon from './img/icons/coffee.svg';
-import beerIcon from './img/icons/beer.svg';
-import calendarIcon from './img/icons/calendar.svg';
+import React, { Component } from "react";
+import Card from "./components/Card";
+import devsAndDrinksImage from "./img/events/devs-and-drinks.png";
+import studyHallImage from "./img/events/study-hall.png";
+import sparkcadeImage from "./img/events/sparkcade.png";
+import coffeeIcon from "./img/icons/coffee.svg";
+import beerIcon from "./img/icons/beer.svg";
+import calendarIcon from "./img/icons/calendar.svg";
 
-let devsAndDrinks = {
+const devsAndDrinks = {
   image: devsAndDrinksImage,
-  link: 'https://calendar.google.com/event?action=TEMPLATE&tmeid=MzZoYW81bHUzNjJxa29jb3N2NHNsbWtib21fMjAxODA0MTFUMDAwMDAwWiAxbzNmcmdzam84anNmZ3JldXE4ZDhucTlqMEBn&tmsrc=1o3frgsjo8jsfgreuq8d8nq9j0%40group.calendar.google.com&scp=ALL',
-  title: 'Devs & Drinks',
-  time: '7-9:30pm > Every Second Tuesday',
-  location: '@ the Pawn & Pint',
-  body: "Come socialize with KC area indie game developers for a night of good beer, food, and board games! Making games can be hard work, so take some time to chill and have fun with other game makers!",
+  link:
+    "https://calendar.google.com/event?action=TEMPLATE&tmeid=MzZoYW81bHUzNjJxa29jb3N2NHNsbWtib21fMjAxODA0MTFUMDAwMDAwWiAxbzNmcmdzam84anNmZ3JldXE4ZDhucTlqMEBn&tmsrc=1o3frgsjo8jsfgreuq8d8nq9j0%40group.calendar.google.com&scp=ALL",
+  title: "Devs & Drinks",
+  time: "7-9:30pm > Every Second Tuesday",
+  location: "@ the Pawn & Pint",
+  body:
+    "Come socialize with KC area indie game developers for a night of good beer, food, and board games! Making games can be hard work, so take some time to chill and have fun with other game makers!"
 };
 
-let studyHall = {
+const studyHall = {
   image: studyHallImage,
-  link: 'https://calendar.google.com/event?action=TEMPLATE&tmeid=bGVlcnFydXBtNDZoMDdkNjhncDJzNDZvdWtfMjAxODA0MDFUMTcwMDAwWiAxbzNmcmdzam84anNmZ3JldXE4ZDhucTlqMEBn&tmsrc=1o3frgsjo8jsfgreuq8d8nq9j0%40group.calendar.google.com&scp=ALL',
-  title: 'Study Hall',
-  time: 'Noon to 5pm > Every Sunday',
-  location: '@ the Broadway Roasting Co.',
-  body: "Come make games with us at the Broadway Roasting Co! We meet every Sunday in the back to co-work on whatever game projects we currently have. It's a great time to make some progress on your game in a friendly environment with other devs.",
+  link:
+    "https://calendar.google.com/event?action=TEMPLATE&tmeid=bGVlcnFydXBtNDZoMDdkNjhncDJzNDZvdWtfMjAxODA0MDFUMTcwMDAwWiAxbzNmcmdzam84anNmZ3JldXE4ZDhucTlqMEBn&tmsrc=1o3frgsjo8jsfgreuq8d8nq9j0%40group.calendar.google.com&scp=ALL",
+  title: "Study Hall",
+  time: "Noon to 5pm > Every Sunday",
+  location: "@ the Broadway Roasting Co.",
+  body:
+    "Come make games with us at the Broadway Roasting Co! We meet every Sunday in the back to co-work on whatever game projects we currently have. It's a great time to make some progress on your game in a friendly environment with other devs."
+};
+
+const sparkcade = {
+  image: sparkcadeImage,
+  link:
+    "https://calendar.google.com/event?action=TEMPLATE&tmeid=N3Y5YTdub202ZGFvdmk2cmZ2cjhpcWV1bzMgMW8zZnJnc2pvOGpzZmdyZXVxOGQ4bnE5ajBAZw&tmsrc=1o3frgsjo8jsfgreuq8d8nq9j0%40group.calendar.google.com",
+  title: "sparKCade",
+  time: "6:30pm to 9pm > Sunday August 19th",
+  location: "@ Strange Days Brewing Co.",
+  body:
+    "sparKCade is a showcase of indie games made by game developers in Kansas City!\
+    Come play games created by local game designers and hang out with the devs at this free event hosted by Flyover Indies and Strange Days Brewing Co."
 };
 
 class Events extends React.Component {
-
   render() {
     return (
       <div className="">
-        <p className='title fi-title'>Upcoming Events</p>
+        <p className="title fi-title">Upcoming Events</p>
 
-        <article className='media fi-media'>
+        <article className="media fi-media">
           <figure className="media-left">
-            <p className="image is-64x64">
-              <img src={coffeeIcon} alt="Study Hall" />
-            </p>
+            <p className="is-size-4">🕹️</p>
+            <p className="is-size-4">🍻</p>
           </figure>
           <div className="media-content">
             <div className="content">
-              <p className='subtitle'>{studyHall.title}</p>
+              <p className="subtitle">{sparkcade.title}</p>
+              <div class="tags has-addons">
+                <span class="tag is-light">{sparkcade.time}</span>
+                <span class="tag is-dark">{sparkcade.location}</span>
+              </div>
+              <p>{sparkcade.body}</p>
+              <a
+                className="button fi-button"
+                href={sparkcade.link}
+                target="_blank"
+              >
+                Add to Calendar
+              </a>
+            </div>
+          </div>
+        </article>
+
+        <article className="media fi-media">
+          <figure className="media-left">
+            <p className="is-size-4">💻</p>
+            <p className="is-size-4">☕</p>
+          </figure>
+          <div className="media-content">
+            <div className="content">
+              <p className="subtitle">{studyHall.title}</p>
               <div class="tags has-addons">
                 <span class="tag is-light">{studyHall.time}</span>
                 <span class="tag is-dark">{studyHall.location}</span>
               </div>
               <p>{studyHall.body}</p>
-              <a className='button fi-button' href={studyHall.link} target='_blank'>Add to Calendar</a>
+              <a
+                className="button fi-button"
+                href={studyHall.link}
+                target="_blank"
+              >
+                Add to Calendar
+              </a>
             </div>
           </div>
         </article>
 
-        <article className='media fi-media'>
+        <article className="media fi-media">
           <figure className="media-left">
-            <p className="image is-64x64">
-              <img src={beerIcon} alt={devsAndDrinks.title} />
-            </p>
+            <p className="is-size-4">🎲</p>
+            <p className="is-size-4">🍻</p>
           </figure>
           <div className="media-content">
             <div className="content">
-              <p className='subtitle'>{devsAndDrinks.title}</p>
+              <p className="subtitle">{devsAndDrinks.title}</p>
               <div class="tags has-addons">
                 <span class="tag is-light">{devsAndDrinks.time}</span>
                 <span class="tag is-dark">{devsAndDrinks.location}</span>
               </div>
               <p>{devsAndDrinks.body}</p>
-              <a className='button fi-button' href={devsAndDrinks.link} target='_blank'>Add to Calendar</a>
+              <a
+                className="button fi-button"
+                href={devsAndDrinks.link}
+                target="_blank"
+              >
+                Add to Calendar
+              </a>
             </div>
           </div>
         </article>
@@ -72,9 +122,8 @@ class Events extends React.Component {
         {/* <Card image={devsAndDrinks.image} link={devsAndDrinks.link} title={devsAndDrinks.title} subtitle={devsAndDrinks.subtitle} /> */}
 
         {/* <Card image={studyHall.image} link={studyHall.link} title={studyHall.title} subtitle={studyHall.subtitle} /> */}
-
       </div>
-     );
+    );
   }
 }
 
