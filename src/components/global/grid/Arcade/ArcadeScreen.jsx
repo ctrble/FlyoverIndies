4203ee54408f0f34691beb17a462d11b04f8b117
Charlotte Trible <ctrble@gmail.com>
2021-05-04 08:21:48 -0500

    yay blurs

--- src/components/global/grid/Arcade/ArcadeScreen.jsx
+++ src/components/global/grid/Arcade/ArcadeScreen.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 import styles from './Arcade.module.scss';
 
 const ArcadeScreen = ({ children }) => (
   <section className={styles.arcade__screen}>
-    <div className={styles.arcade__screenContent}>{children}</div>
+    <div className={styles.arcade__screenContent}>
+      <div
+        className={classnames(
+          styles.arcade__screenBlur,
+          styles.arcade__screenBlur_top
+        )}
+      />
+      {children}
+      <div
+        className={classnames(
+          styles.arcade__screenBlur,
+          styles.arcade__screenBlur_bottom
+        )}
+      />
+    </div>
   </section>
 );
 
