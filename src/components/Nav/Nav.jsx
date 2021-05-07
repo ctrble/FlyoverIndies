7797300ce8afdb6cfd0c54e30d92649b968171cc
Charlotte Trible <ctrble@gmail.com>
2021-05-07 08:32:00 -0500

    styling some links

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,16 +4,20 @@ import { v4 as uuidv4 } from 'uuid';
 
 import navData from 'data/nav';
 
+import styles from './Nav.module.scss';
+
 const Nav = () => (
-  <ul>
-    {navData.pages.map((item) => (
-      <li key={uuidv4()}>
-        <Link href={item.path}>
-          <a>{item.title}</a>
-        </Link>
-      </li>
-    ))}
-  </ul>
+  <nav className={styles.nav}>
+    <ul className={styles.nav__list}>
+      {navData.pages.map((item) => (
+        <li key={uuidv4()}>
+          <Link href={item.path}>
+            <a className={styles.nav__link}>{item.title}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </nav>
 );
 
 export default Nav;
