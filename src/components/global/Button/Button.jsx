cf7394ca13a0c2cfae3becf54ae069cfdafced0f
Charlotte Trible <ctrble@gmail.com>
2021-05-16 20:03:42 -0500

    using new links in buttons

--- src/components/global/Button/Button.jsx
+++ src/components/global/Button/Button.jsx
@@ -1,27 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import Link from 'next/link';
-import is from 'is_js';
+
+import SiteLink from 'src/components/global/SiteLink';
 
 import styles from './Button.module.scss';
 
 /* eslint-disable react/jsx-props-no-spreading */
 const Button = ({ children, href, ...props }) => {
   if (href) {
-    if (is.url(href)) {
-      return (
-        <a className={classnames(styles.button)} href={href} {...props}>
-          {children}
-        </a>
-      );
-    }
     return (
-      <Link href={href}>
-        <a className={classnames(styles.button)} {...props}>
-          {children}
-        </a>
-      </Link>
+      <SiteLink className={styles.button} href={href} {...props}>
+        {children}
+      </SiteLink>
     );
   }
 
