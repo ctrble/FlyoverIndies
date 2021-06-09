6420c34faec265942b46ff106d6f84841c5482ad
Charlotte Trible <ctrble@gmail.com>
2021-06-09 18:42:00 -0500

    oh boy navs

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import classnames from 'classnames';
 import { useRouter } from 'next/router';
 import { v4 as uuidv4 } from 'uuid';
+import Hamburger from 'hamburger-react';
+import { useMediaQuery } from 'react-responsive';
 
 import navData from 'data/nav';
 
@@ -11,30 +13,56 @@ import styles from './Nav.module.scss';
 
 const Nav = () => {
   const router = useRouter();
-  const isActive = (path) => router.asPath === path;
+  const [isOpen, setOpen] = useState(false);
+  const isPortrait = useMediaQuery({
+    query: '(orientation: portrait)',
+  });
 
-  const handleClick = (event) => {
-    event.preventDefault();
-  };
+  useEffect(() => {
+    const handleRouteChange = () => setOpen(false);
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  // Prevent reloading of active pages
+  const handleClick = (event) => event.preventDefault();
+  const isActive = (path) => router.asPath === path;
 
   return (
     <nav className={styles.nav}>
-      <ul className={styles.nav__list}>
-        {navData.pages.map((item) => (
-          <li key={uuidv4()}>
-            <SiteLink
-              className={classnames(
-                styles.nav__link,
-                isActive(item.path) && styles.nav__link_isActive
-              )}
-              href={item.path}
-              onClick={isActive(item.path) ? handleClick : null}
-            >
-              {item.title}
-            </SiteLink>
-          </li>
-        ))}
-      </ul>
+      {isPortrait && (
+        <Hamburger
+          toggled={isOpen}
+          toggle={setOpen}
+          size="48"
+          distance="lg"
+          label="Show menu"
+          color={styles.var_burgerColor}
+        />
+      )}
+      {(isOpen || !isPortrait) && (
+        <ul className={styles.nav__list}>
+          {navData.pages.map((item) => (
+            <li className={styles.nav__item} key={uuidv4()}>
+              <SiteLink
+                className={classnames(
+                  styles.nav__link,
+                  isActive(item.path) && styles.nav__link_isActive
+                )}
+                href={item.path}
+                // tabIndex="0"
+                onClick={isActive(item.path) ? handleClick : null}
+              >
+                {item.title}
+              </SiteLink>
+            </li>
+          ))}
+        </ul>
+      )}
     </nav>
   );
 };
