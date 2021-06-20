84b97a18f0ff564bb6a8fb2caf16c34e5f98ab32
Charlotte Trible <ctrble@gmail.com>
2021-06-20 10:27:03 -0500

    so many upgrades

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 // for transpiling all ESM @fullcalendar/* packages
 // also, for piping fullcalendar thru babel (to learn why, see babel.config.js)
+// declare withTM as your last plugin (the "most nested" one)
 const withTM = require('next-transpile-modules')([
   '@fullcalendar/common',
   '@fullcalendar/google-calendar',
