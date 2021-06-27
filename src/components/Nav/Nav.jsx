4d1b2ba7759ba79e2aeca904a4ddef7d3cdf51e4
Charlotte Trible <ctrble@gmail.com>
2021-06-27 15:57:53 -0500

    animate the nav a bit

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import { v4 as uuidv4 } from 'uuid';
 import Hamburger from 'hamburger-react';
 import { useMediaQuery } from 'react-responsive';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import navData from 'data/nav';
 
@@ -48,25 +49,34 @@ const Nav = () => {
           color={styles.var_burgerColor}
         />
       )}
-      {(isOpen || !showBurger) && (
-        <ul className={styles.nav__list}>
-          {navData.pages.map((item) => (
-            <li className={styles.nav__item} key={uuidv4()}>
-              <SiteLink
-                className={classnames(
-                  styles.nav__link,
-                  isActive(item.path) && styles.nav__link_isActive
-                )}
-                href={item.path}
-                // tabIndex="0"
-                onClick={isActive(item.path) ? handleClick : null}
-              >
-                {item.title}
-              </SiteLink>
-            </li>
-          ))}
-        </ul>
-      )}
+      <AnimatePresence>
+        {(isOpen || !showBurger) && (
+          <motion.ul
+            className={styles.nav__list}
+            key="navigation"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.4 }}
+          >
+            {navData.pages.map((item) => (
+              <li className={styles.nav__item} key={uuidv4()}>
+                <SiteLink
+                  className={classnames(
+                    styles.nav__link,
+                    isActive(item.path) && styles.nav__link_isActive
+                  )}
+                  href={item.path}
+                  // tabIndex="0"
+                  onClick={isActive(item.path) ? handleClick : null}
+                >
+                  {item.title}
+                </SiteLink>
+              </li>
+            ))}
+          </motion.ul>
+        )}
+      </AnimatePresence>
     </nav>
   );
 };
