4469bd244f114233b8818cfa109e1b0820c4f803
Charlotte Trible <ctrble@gmail.com>
2021-07-11 16:41:12 -0500

    adding GA

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
 import { DefaultSeo } from 'next-seo';
 import { SWRConfig } from 'swr';
+import { useRouter } from 'next/router';
 
+import * as gtag from 'src/lib/gtag';
 import SEO from 'next-seo.config';
 
 import 'src/scss/global.scss';
@@ -10,6 +12,17 @@ import 'src/scss/global.scss';
 /* eslint-disable react/prop-types, react/jsx-props-no-spreading */
 function FlyoverIndies({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
+
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
